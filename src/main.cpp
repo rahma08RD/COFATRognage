6615e1e4e776e10561108d4d03d720641edb43f3
Rahma Dridi <dridirahma08@gmail.com>
2023-04-06 12:21:45 +0000

    programme modifié

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
+// PIN_21 = broche de commande du moteur secondaire
+// PIN_17 = broche de commande du boutton poussoir S1
+// PIN_5 = broche de commande de l'électrovanne
+// PIN_3 = broche de commande de la sortie du vèrin 
+// PIN_4 = broche de commande de recul du vèrin 
+// PIN_19 = broche de commande du moteur principale
+// PIN_2 = broche de commande de la capteur de sécurité du capot 
+// PIN_22 = broche de commande de la boutton départ cycle
 #include <Arduino.h>
 
 //Déclaration des broches d'entrée/sortie
-const int MOTOR2_PIN = 21;
+const int MOTORsec_PIN = 21;
 const int s1 =17;
 const int electrovanne_pin = 5;// broche de commande de l'électrovanne
-const int VerinAPin = 3;
-const int VerinBPin = 4;
-const int MOTOR1_PIN = 19;
+const int VerinSORTIEPin = 3;
+const int VerinRECULEPin = 4;
+const int MOTORprincip_PIN = 19;
 const int cp = 2;
 const int dcy = 22;
 
@@ -19,16 +27,16 @@ bool dcyState = 0;         // variable for reading the pushbutton status
 
 void setup() {
   // put your setup code here, to run once:
-    pinMode(MOTOR2_PIN, OUTPUT);
+    
     pinMode(s1, INPUT);
-    pinMode(MOTOR1_PIN, OUTPUT);
     pinMode(dcy, INPUT);
     pinMode(cp, INPUT);
 
-
+    pinMode(MOTORprincip_PIN, OUTPUT);
+    pinMode(MOTORsec_PIN, OUTPUT);
     pinMode(electrovanne_pin, OUTPUT);
-    pinMode(VerinAPin, OUTPUT);
-    pinMode(VerinBPin, OUTPUT);
+    pinMode(VerinSORTIEPin, OUTPUT);
+    pinMode(VerinRECULEPin, OUTPUT);
 }
 
 void loop() {
@@ -49,26 +57,26 @@ void loop() {
       
      if (dcyState == HIGH && cpState==HIGH )  
      {{
-    digitalWrite(MOTOR1_PIN, HIGH); // Keep the motor on
+    digitalWrite(MOTORprincip_PIN, HIGH); // Keep the motor on
     digitalWrite(electrovanne_pin, HIGH);
      }
      if (dcyState == HIGH && cpState==HIGH && s1State==HIGH )  
      {
-    digitalWrite(MOTOR2_PIN, HIGH); 
-    digitalWrite(VerinAPin, HIGH);
-    digitalWrite(VerinBPin, LOW);
+    digitalWrite(MOTORsec_PIN, HIGH); 
+    digitalWrite(VerinSORTIEPin, HIGH);
+    digitalWrite(VerinRECULEPin, LOW);
     delay(3000); //Attend de 3 secondes pour que le vérin Éteint la position souhaitée
-    digitalWrite(MOTOR2_PIN, LOW);
+    digitalWrite(MOTORsec_PIN, LOW);
     digitalWrite(electrovanne_pin, LOW); // Désactiver l'électrovanne
-    digitalWrite(VerinAPin, LOW); //Commande pour reculer le vérin
-    digitalWrite(VerinBPin, HIGH);
+    digitalWrite(VerinSORTIEPin, LOW); 
+    digitalWrite(VerinRECULEPin, HIGH); //Commande pour reculer le vérin
      }}
     else {
-    digitalWrite(MOTOR1_PIN, LOW);
-    digitalWrite(MOTOR2_PIN, LOW);
-    digitalWrite(electrovanne_pin, LOW); // Désactiver l'électrovanne
-    digitalWrite(VerinAPin, LOW); //Commande pour reculer le vérin
-    digitalWrite(VerinBPin, LOW); 
+    digitalWrite(MOTORprincip_PIN, LOW);
+    digitalWrite(MOTORsec_PIN, LOW);
+    digitalWrite(electrovanne_pin, LOW); 
+    digitalWrite(VerinSORTIEPin, LOW); 
+    digitalWrite(VerinRECULEPin, LOW); 
     }
 
 }
